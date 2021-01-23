cf012ec6244a22ead105ad166187e6f793bd1ecc
Charles Ferguson <charles@webdevjm.com>
2021-01-23 16:18:43 +0000

    Added my photos

---
+++ photos/index.php
@@ -0,0 +1,56 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta name="viewport" content="width=device-width initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>Photographs</title>
+    <link rel="stylesheet" href="/css/master.css">
+    <link rel="stylesheet" href="/css/mobile.css">
+    <link rel="stylesheet" href="/css/desktop.css" media="only screen and (min-width: 1000px) and (orientation:landscape)">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <header>
+      <h1>Photographs</h1>
+      <h3>By Charles Ferguson</h3>
+    </header>
+    <div class="about">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div class="photo-year">
+      <h3>2017</h3>
+    </div>
+    <div class="photo-row">
+
+      <div class="photo-column">
+        <img src="img/p1.jpg" alt="">
+        <img src="img/p4.jpg" alt="">
+        <img src="img/p10.jpg" alt="">
+        <img src="img/p13.jpg" alt="">
+      </div>
+      <div class="photo-column">
+        <img src="img/p2.jpg" alt="">
+        <img src="img/p5.jpg" alt="">
+        <img src="img/p8.jpg" alt="">
+        <img src="img/p11.jpg" alt="">
+
+      </div>
+      <div class="photo-column">
+        <img src="img/p3.jpg" alt="">
+        <img src="img/p6.jpg" alt="">
+        <img src="img/p9.jpg" alt="">
+        <img src="img/p12.jpg" alt="">
+        <!--<img src="img/p7.jpg" alt="">-->
+
+      </div>
+
+    </div>
+    <div class="sm-tag">
+      <a href="https://instagram.com/ferguson.today" target="_blank"><span class="f-sm-link fa fa-instagram"></span></a>
+    </div>
+
+  </body>
+</html>
