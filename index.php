2094cac452fc07ca18890dc0b21f13b94b98fc49
Charles Ferguson <charles@webdevjm.com>
2021-01-23 16:27:53 +0000

    Redirect to photos page

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-<?php echo 'Hello World';
+<?php echo '<script type"text/javascript">window.location.replace("/photos");</script>';
 /*This page will contain links to blog, photos and programs and a short about me and links to external platforms*/
 ?>
